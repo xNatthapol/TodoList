9c866a6a8946188b31d92405c391e6f0441355c9
Natthapol SERMSARAN <natthapolwork@gmail.com>
2025-04-19 23:57:15 +0700

    Add database setup

---
+++ backend/internal/database/database.go
@@ -0,0 +1,42 @@
+package database
+
+import (
+	"fmt"
+	"github.com/xNatthapol/todo-list/internal/config"
+	"github.com/xNatthapol/todo-list/internal/models"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+func ConnectDB(cfg *config.Config) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Bangkok",
+		cfg.DBHost, cfg.DBUser, cfg.DBPassword, cfg.DBName, cfg.DBPort, cfg.DBSSLMode)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Info),
+	})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect database: %w", err)
+	}
+
+	log.Println("Database connection established")
+
+	// Run migrations
+	log.Println("Running database migrations...")
+	err = db.AutoMigrate(&models.User{}, &models.Todo{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to migrate database: %w", err)
+	}
+	log.Println("Database migrated successfully")
+
+	// Assign to global variable
+	DB = db
+
+	return db, nil
+}
